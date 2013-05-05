3d8a06f7c7cac61283d3208f75d2192da8adebdd
Pras Velagapudi <psigen@gmail.com>
2013-05-05 00:05:07 -0400

    Ok, workign excepto for the serialization part.

--- bson_streamer/src/pcl_stream.cpp
+++ bson_streamer/src/pcl_stream.cpp
@@ -17,21 +17,44 @@ using boost::asio::ip::tcp;
 std::vector<boost::shared_ptr<tcp::iostream> > _streams;
 boost::mutex _stream_lock;
 
+void broadcast(mongo::BSONObj obj) 
+{
+    boost::lock_guard<boost::mutex> lock(_stream_lock);
+    BOOST_FOREACH( boost::shared_ptr<tcp::iostream> stream, _streams ) {
+      (*stream) << obj.objdata();
+      stream->flush();
+    }
+}
+
 void pclCallback(const sensor_msgs::PointCloud2ConstPtr msg) 
 {
   // TODO: create a BSON message and send it out
   mongo::BSONObjBuilder b;
   b.append("name", "Joe");
   b.append("age", 33);
-  mongo::BSONObj bson_points = b.obj();
+  mongo::BSONObj bson_obj = b.obj();
 
   // Send this data to each client
-  {
-    boost::lock_guard<boost::mutex> lock(_stream_lock);
-    BOOST_FOREACH( boost::shared_ptr<tcp::iostream> stream, _streams ) {
-      (*stream) << bson_points.objdata();
-      stream->flush();
-    }
+  broadcast(bson_obj);
+}
+
+void serverThreadFn(boost::asio::io_service& io_service, int port) 
+{
+  tcp::endpoint endpoint(tcp::v4(), port);
+  tcp::acceptor acceptor(io_service, endpoint);
+  
+  while (ros::ok()) {
+    boost::shared_ptr<tcp::iostream> stream = boost::make_shared<tcp::iostream>();
+    boost::system::error_code ec;
+    
+    acceptor.accept(*stream->rdbuf(), ec);
+    if (ec) {
+      ROS_WARN("Connection failed: %s", ec.message().c_str());
+      continue;
+    } else {
+      boost::lock_guard<boost::mutex> lock(_stream_lock);
+      _streams.push_back(stream);
+    }    
   }
 }
 
@@ -44,29 +67,19 @@ int main(int argc, char **argv)
   // Subscribe to a point cloud stream
   ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("points", 1, pclCallback);
 
-  // Start a ROS handling thread
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-
   // Open a TCP socket
   int stream_port;
   nh.param<int>("port", stream_port, 6666);
   boost::asio::io_service io_service;
-  tcp::endpoint endpoint(tcp::v4(), stream_port);
-  tcp::acceptor acceptor(io_service, endpoint);
+  boost::thread server_thread(serverThreadFn, boost::ref(io_service), stream_port);
 
-  while(ros::ok()) {
-    boost::shared_ptr<tcp::iostream> stream = boost::make_shared<tcp::iostream>();
-    boost::system::error_code ec;
+  // Update ROS
+  ROS_INFO("Starting BSON stream: %s -> [%d]", 
+	   nh.resolveName("points").c_str(), stream_port);
+  ros::spin();
+  ROS_INFO("Stopping BSON stream: %s -> [%d]", 
+	   nh.resolveName("points").c_str(), stream_port);
 
-    acceptor.accept(*stream->rdbuf(), ec);
-    if (ec) {
-      ROS_INFO("Connection failed: %s", ec.message().c_str());
-      continue;
-    } else {
-      boost::lock_guard<boost::mutex> lock(_stream_lock);
-      _streams.push_back(stream);
-    }
-  }
+  // Shutdown boost.asio
+  io_service.stop();
 }
-
