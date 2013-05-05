1bece657c43b536b6c3850cd5ae9e1947fba832f
Pras Velagapudi <psigen@gmail.com>
2013-05-04 23:17:13 -0400

    Added compileable stream code.

--- bson_streamer/src/pcl_stream.cpp
+++ bson_streamer/src/pcl_stream.cpp
@@ -6,32 +6,38 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "mongo/bson/bson.h"
+#include <boost/asio.hpp>
 #include "boost/thread/thread.hpp"
 #include "boost/thread/locks.hpp"
 #include <vector>
 
 using boost::asio::ip::tcp; 
 
-std::vector<tcp::iostream> _streams;
+std::vector<boost::shared_ptr<tcp::iostream> > _streams;
 boost::mutex _stream_lock;
 
-void pclCallback(const sensor_msgs::PointCloud2& msg) 
+void pclCallback(const sensor_msgs::PointCloud2ConstPtr msg) 
 {
   // TODO: create a BSON message and send it out
 
   {
-    boost::lock_guard<boost::mutex>(_stream_lock);
+    boost::lock_guard<boost::mutex> lock(_stream_lock);
     // TODO: send this to everyone.
   }
 }
 
 int main(int argc, char **argv)
 {
+  // Initialize ROS node
   ros::init(argc, argv, "pcl_stream");
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("points", pclCallback);
-  // TODO: run an async spinner here
+  // Subscribe to a point cloud stream
+  ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("points", 1, pclCallback);
+
+  // Start a ROS handling thread
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
   // Open a TCP socket
   boost::asio::io_service io_service;
@@ -39,12 +45,12 @@ int main(int argc, char **argv)
   tcp::acceptor acceptor(io_service, endpoint);
 
   while(ros::ok()) {
-    tcp::iostream stream;
+    boost::shared_ptr<tcp::iostream> stream = boost::make_shared<tcp::iostream>();
     boost::system::error_code ec;
 
-    acceptor.accept(*stream.rdbuf(), ec);
+    acceptor.accept(*stream->rdbuf(), ec);
     if (ec) {
-      ROS_INFO("Connection failed: %d", ec);
+      ROS_INFO("Connection failed: %s", ec.message().c_str());
       continue;
     } else {
       boost::lock_guard<boost::mutex> lock(_stream_lock);
