87d114d891837ba4800a9a213073f1b5d9ead3df
Pras Velagapudi <psigen@gmail.com>
2013-05-07 00:40:57 -0400

    Added updated manifest and hydra converter to take into account hydra messages.

--- kdl_hydra/src/kdl_hydra.py
+++ kdl_hydra/src/kdl_hydra.py
@@ -7,12 +7,21 @@
 import roslib; roslib.load_manifest('kdl_hydra')
 import rospy
 
+from tf import TransformListener
+from razer_hydra.msg import Hydra
 from pykdl_utils import kdl_parser
 from urdf_parser_py.urdf import URDF
-#import PyKDL as kdl
+import PyKDL as kdl
+
+
+def hydra_callback(hydra_msg):
+    pass
 
 def main():
     rospy.init_node('kdl_hydra')
+
+    # Create a transform listener
+    tf = TransformListener()
     
     # Retrieve raw robot parameters from rosmaster
     robot_string = rospy.get_param("robot_description")
@@ -24,6 +33,9 @@ def main():
 
     # Load URDF model of robot description into KDL
     robot_kdl = kdl_parser.kdl_tree_from_urdf_model(robot_urdf)
+
+    # Subscribe to hydra updates
+    rospy.Subscriber("hydra_calib", Hydra, hydra_callback)
 
     # Start main event handling loop
     rospy.loginfo('Started kdl_hydra node...')
