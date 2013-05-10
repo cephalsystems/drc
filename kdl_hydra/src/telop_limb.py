246f700136b77a7402bf3f801b6e0941bbb67383
Pras Velagapudi <psigen@gmail.com>
2013-05-09 23:10:12 -0400

    Added fix for locking in teleop limb updates.

--- kdl_hydra/src/telop_limb.py
+++ kdl_hydra/src/telop_limb.py
@@ -88,47 +88,46 @@ class TeleopLimb:
                           for name in self._name_chain ]
 
     def update(self, atlas_state):
-
-        # Initialize current configurations when we get our first message
-        if not self._q_current:
-            self._q_current = kdl.JntArray(self._num_joints)
-
-        if not self._q_desired:
-            self._q_desired = kdl.JntArray(self._q_current)
-
-        # Populate current configuration from state message
         with _lock:
+            # Initialize current configurations when we get our first message
+            if not self._q_current:
+                self._q_current = kdl.JntArray(self._num_joints)
+                
+            if not self._q_desired:
+                self._q_desired = kdl.JntArray(self._q_current)
+
+            # Populate current configuration from state message
             for idx in _idx_chain:
                 self._q_current = atlas_msg.position[idx]
 
 
     def solve(self, tf_target):
-
         try:
-            # Compute the relative transform to the end link
-            time = self._tf.getLatestCommonTime("/" + self._name_chain[0], tf_target)
-            pos, quat = self._tf.lookupTransform("/" + self._name_chain[0], tf_target, time)
-            x_desired = kdl.Frame(kdl.Rotation.Quaternion(_quat[0], _quat[1], _quat[2], _quat[3]),
-                                  kdl.Vector(_pos[0], _pos[1], _pos[2]))
-
-            # Compute IK for desired joint positions
-            q_desired = kdl.JntArray(self._q_desired)
-            success = self._ik.CartToJnt(self_q.current, x_desired, q_desired)
-            if (success >= 0):
-                self._x_desired = x_desired
-                self._q_desired = q_desired
-            else:
-                raise Exception('No IK for %s [%d]'
-                                .format(self._name_chain[-1], success))
+            with _lock:
+                # Compute the relative transform to the end link
+                time = self._tf.getLatestCommonTime("/" + self._name_chain[0], tf_target)
+                pos, quat = self._tf.lookupTransform("/" + self._name_chain[0], tf_target, time)
+                x_desired = kdl.Frame(kdl.Rotation.Quaternion(_quat[0], _quat[1], _quat[2], _quat[3]),
+                                      kdl.Vector(_pos[0], _pos[1], _pos[2]))
+                
+                # Compute IK for desired joint positions
+                q_desired = kdl.JntArray(self._q_desired)
+                success = self._ik.CartToJnt(self_q.current, x_desired, q_desired)
+                if (success >= 0):
+                    self._x_desired = x_desired
+                    self._q_desired = q_desired
+                else:
+                    raise Exception('No IK for %s [%d]'
+                                    .format(self._name_chain[-1], success))
 
         except Exception as e:
             rospy.logwarn('IK failed: %s'.format(e))
-
+        
 
     def populate(self, atlas_command):
-
-        # Fill in relevant components of command to atlas
-        for idx in range(0, self._num_joints):
-            atlas_command.position[self._idx_chain[idx]] = self._q_desired[idx]
-            atlas_command.k_effort[self._idx_chain[idx]] = 255
+        with _lock:
+            # Fill in relevant components of command to atlas
+            for idx in range(0, self._num_joints):
+                atlas_command.position[self._idx_chain[idx]] = self._q_desired[idx]
+                atlas_command.k_effort[self._idx_chain[idx]] = 255
 
