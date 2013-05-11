d49d9f6e967f4948b993d860e16cd547593b5a58
Pras Velagapudi <psigen@gmail.com>
2013-05-11 16:11:56 -0400

    Added working handlers for hydra initial stuff and a TF publisher that can be arbitrarily modified.

--- super_teleop/src/teleop.cpp
+++ super_teleop/src/teleop.cpp
@@ -1,10 +1,15 @@
 #include "teleop_robot.hpp"
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 
 #include <razer_hydra/Hydra.h>
 #include <atlas_msgs/AtlasState.h>
 #include <atlas_msgs/AtlasCommand.h>
 #include <pi_tracker/Skeleton.h>
+
+tf::TransformBroadcaster br;
+tf::TransformListener tl;
 
 void atlasCallback(const atlas_msgs::AtlasState::ConstPtr &msg)
 {
@@ -13,7 +18,25 @@ void atlasCallback(const atlas_msgs::AtlasState::ConstPtr &msg)
 
 void hydraCallback(const razer_hydra::Hydra::ConstPtr &msg)
 {
+  // Transform and scale hydra poses as necessary
+  std::string paddle_tf[] = {"/left", "/right"};
+  for (int i = 0; i < 2; ++i)
+  {
+    tf::Transform transform;
+    tf::transformMsgToTF(msg->paddles[i].transform, transform);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), 
+					  "/utorso", paddle_tf[i]));
+  }
 
+  // Send command to left arm
+  if (msg->paddles[0].trigger > 0.9) {
+    
+  }
+
+  // Send command to right arm
+  if (msg->paddles[1].trigger > 0.9) {
+
+  }
 }
 
 void skeletonCallback(const pi_tracker::Skeleton::ConstPtr &msg)
--- super_teleop/src/teleop_robot.hpp
+++ super_teleop/src/teleop_robot.hpp
@@ -2,8 +2,15 @@
 #define __TELEOP_ROBOT_HPP__
 
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
+
 #include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chainiksolver.hpp>
+#include <kdl/chainfksolver.hpp>
 #include <urdf/model.h>
+
+#include <string>
+#include <map>
 
 class TeleopRobot
 {
@@ -12,6 +19,28 @@ public:
 
   urdf::Model urdf;
   KDL::Tree kdl;
+};
+
+class TeleopLimb
+{
+public:
+  TeleopLimb(TeleopRobot &robot, std::string start_link, std::string end_link);
+
+  void update(std::map<std::string, double> pos);
+
+  std::map<std::string, double> solveEnd(tf::Transform end);
+  std::map<std::string, double> solveLimb(std::map<std::string, tf::Transform> limb);
+  std::map<std::string, double> clear();
+
+private:
+  int n_joints_;
+  std::map<std::string, int> names_to_idx_;
+  std::vector<std::string> idx_to_names_;
+  KDL::JntArray q_current_;
+
+  KDL::ChainFkSolverPos *fk_;
+  KDL::ChainIkSolverVel *ikv_;
+  KDL::ChainIkSolverPos *ik_;
 };
 
 #endif /* __TELEOP_ROBOT_HPP__ */
