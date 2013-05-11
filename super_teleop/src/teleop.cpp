9e73ef9493ae06934f99c73265d5463638296b83
Pras Velagapudi <psigen@gmail.com>
2013-05-11 17:50:02 -0400

    Got compileable reading of ATLAS state messages!

--- super_teleop/src/teleop.cpp
+++ super_teleop/src/teleop.cpp
@@ -9,25 +9,46 @@
 #include <atlas_msgs/AtlasCommand.h>
 #include <pi_tracker/Skeleton.h>
 
+#include <vector>
+#include <map>
+#include <boost/foreach.hpp>
+
 tf::TransformBroadcaster br;
 tf::TransformListener tl;
 std::vector<TeleopLimb> limbs;
 
+std::vector<std::string> ATLAS_JOINT_NAMES = {
+  "back_lbz", "back_mby", "back_ubx", "neck_ay",
+  "l_leg_uhz", "l_leg_mhx", "l_leg_lhy", "l_leg_kny", "l_leg_uay", "l_leg_lax",
+  "r_leg_uhz", "r_leg_mhx", "r_leg_lhy", "r_leg_kny", "r_leg_uay", "r_leg_lax",
+  "l_arm_usy", "l_arm_shx", "l_arm_ely", "l_arm_elx", "l_arm_uwy", "l_arm_mwx",  
+  "r_arm_usy", "r_arm_shx", "r_arm_ely", "r_arm_elx", "r_arm_uwy", "r_arm_mwx" 
+  };
+
 void atlasCallback(const atlas_msgs::AtlasState::ConstPtr &msg)
 {
+  // Construct a mapping from limbs to positions
+  std::map<std::string, double> joints;
+  for (unsigned int i = 0; i < ATLAS_JOINT_NAMES.size(); ++i) {
+    joints[ATLAS_JOINT_NAMES[i]] = msg->position[i];
+  }
 
+  // Update each limb with the new positions
+  BOOST_FOREACH(TeleopLimb &limb, limbs) {
+    limb.update(joints);
+  }
 }
 
 void hydraCallback(const razer_hydra::Hydra::ConstPtr &msg)
 {
   // Transform and scale hydra poses as necessary
   std::string paddle_tf[] = {"/left", "/right"};
-  for (int i = 0; i < 2; ++i)
+  for (unsigned int i = 0; i < 2; ++i)
   {
     tf::Transform transform;
     tf::transformMsgToTF(msg->paddles[i].transform, transform);
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), 
-					  "/utorso", paddle_tf[i]));
+					  "/" + limbs[i].startLink(), paddle_tf[i]));
   }
 
   // Send command to left arm
