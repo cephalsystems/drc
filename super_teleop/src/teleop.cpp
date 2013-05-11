2ec5876c7d209ea54e3c5f16e1a98adaa0b5b227
Pras Velagapudi <psigen@gmail.com>
2013-05-11 18:21:30 -0400

    Compileable version of position-based IK solving, I hope.

--- super_teleop/src/teleop.cpp
+++ super_teleop/src/teleop.cpp
@@ -16,6 +16,7 @@
 tf::TransformBroadcaster br;
 tf::TransformListener tl;
 std::vector<TeleopLimb> limbs;
+std::map<std::string, double> commands;
 
 std::vector<std::string> ATLAS_JOINT_NAMES = {
   "back_lbz", "back_mby", "back_ubx", "neck_ay",
@@ -49,16 +50,12 @@ void hydraCallback(const razer_hydra::Hydra::ConstPtr &msg)
     tf::transformMsgToTF(msg->paddles[i].transform, transform);
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), 
 					  "/" + limbs[i].startLink(), paddle_tf[i]));
-  }
 
-  // Send command to left arm
-  if (msg->paddles[0].trigger > 0.9) {
-    
-  }
-
-  // Send command to right arm
-  if (msg->paddles[1].trigger > 0.9) {
-
+    // Add command to arm if trigger is pressed
+    if (msg->paddles[i].trigger > 0.9) {
+      std::map<std::string, double> cmds = limbs[i].solveEnd(transform);
+      commands.insert(cmds.begin(), cmds.end());
+    }
   }
 }
 
@@ -86,6 +83,64 @@ int main(int argc, char* argv[])
   nh.subscribe("/arms/hydra_calib", 1, hydraCallback);
   nh.subscribe("/skeleton", 1, skeletonCallback);
 
-  // Spin ROS node until shutdonw
-  ros::spin();
+  // Create an ATLAS command
+  unsigned int n = ATLAS_JOINT_NAMES.size();
+  atlas_msgs::AtlasCommand cmd_msg;
+  cmd_msg.position.resize(n);
+  cmd_msg.velocity.resize(n);
+  cmd_msg.effort.resize(n);
+  cmd_msg.kp_position.resize(n);
+  cmd_msg.ki_position.resize(n);
+  cmd_msg.kd_position.resize(n);
+  cmd_msg.kp_velocity.resize(n);
+  cmd_msg.i_effort_min.resize(n);
+  cmd_msg.i_effort_max.resize(n);
+
+  // Fill in PID values
+  std::string gains_prefix = "atlas_controller/gains/";
+  for (unsigned int i = 0; i < n; ++i) {
+    double kp, ki, kd, i_clamp;
+
+    nh.getParam("atlas_controller/gains/" + ATLAS_JOINT_NAMES[i] + "/p", kp);
+    nh.getParam("atlas_controller/gains/" + ATLAS_JOINT_NAMES[i] + "/i", ki);
+    nh.getParam("atlas_controller/gains/" + ATLAS_JOINT_NAMES[i] + "/d", kd);
+    nh.getParam("atlas_controller/gains/" + ATLAS_JOINT_NAMES[i] + "/i_clamp", i_clamp);
+
+    cmd_msg.kp_position[i]  = kp;
+    cmd_msg.ki_position[i]  = ki;
+    cmd_msg.kd_position[i]  = kd;
+    cmd_msg.kp_velocity[i]  = 0;
+
+    cmd_msg.i_effort_min[i] = -i_clamp;
+    cmd_msg.i_effort_max[i] =  i_clamp;
+    
+    cmd_msg.position[i]     = 0;
+    cmd_msg.velocity[i]     = 0;
+    cmd_msg.effort[i]       = 0;
+  }
+
+  // Send ATLAS commands at a fixed rate
+  ros::Rate r(100);
+  while(ros::ok()) {
+    
+    // Fill in command from current joint table
+    for (unsigned int i = 0; i < ATLAS_JOINT_NAMES.size(); ++i) {
+
+      // Look for a command for this joint
+      std::map<std::string, double>::iterator it = 
+	commands.find(ATLAS_JOINT_NAMES[i]);
+      
+      // If it exists, fill it in
+      if (it != commands.end()) {
+	cmd_msg.position[i] = it->second;
+	cmd_msg.effort[i] = 255;
+      } else {
+	cmd_msg.effort[i] = 0;
+      }
+    }
+
+    // Wait for next cycle
+    ros::spinOnce();
+    r.sleep();
+  }
 }
