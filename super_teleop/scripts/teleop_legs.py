1ff316509ee246988232a107af035d6234dfc66c
Pras Velagapudi <psigen@gmail.com>
2013-06-13 22:29:57 -0400

    Modified teleop legs to not suck as much.

--- super_teleop/scripts/teleop_legs.py
+++ super_teleop/scripts/teleop_legs.py
@@ -7,43 +7,67 @@ from atlas_msgs.msg import AtlasSimInterfaceCommand, \
                            AtlasBehaviorWalkParams, \
                            AtlasBehaviorStandParams
 from std_msgs.msg import Header
-from razer_hydra.msg import Hydra
-
 from geometry_msgs.msg import Pose
 from std_msgs.msg import String
-from visualization_msgs.msg import MarkerArray, Marker
+from std_msgs.msg import Empty
+from super_teleop.srv import Walk
 
-import tf
-from tf.transformations import quaternion_from_euler
-from tf.transformations import euler_from_quaternion
-
+import sys
 import math
 import rospy
 import itertools
-
-PADDLE_NAMES = [ 'hydra_left', 'hydra_right' ]
-NUM_STEPS = 40
-
+from tf.transformations import quaternion_from_euler
 
 def grouper(n, iterable, fillvalue=None):
     "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return itertools.izip_longest(*args, fillvalue=fillvalue)
 
+class AtlasTeleop(object):
 
-class AtlasTeleop():
+    # Keyboard teleop bindings
+    dynamic_dir = {'u': {"forward":1, "lateral":0, "turn": 1}, \
+                  'i': {"forward":1, "lateral":0, "turn": 0}, \
+                  'o': {"forward":1, "lateral":0, "turn":-1}, \
+                  'j': {"forward":0, "lateral":1, "turn": 0}, \
+                  'k': {"forward":0, "lateral":0, "turn": 0}, \
+                  'l': {"forward":0, "lateral":-1, "turn": 0}, \
+                  'm': {"forward":0, "lateral":0, "turn": 0.5}, \
+                  ',': {"forward":-0.5, "lateral":0, "turn": 0}, \
+                  '.': {"forward":0, "lateral":0, "turn":-0.5}}
+
+    static_dir = {'U': {"forward":1, "lateral":0, "turn": 1}, \
+                  'I': {"forward":1, "lateral":0, "turn": 0}, \
+                  'O': {"forward":1, "lateral":0, "turn":-1}, \
+                  'J': {"forward":0, "lateral":1, "turn": 0}, \
+                  'K': {"forward":-1, "lateral":0, "turn": 0}, \
+                  'L': {"forward":0, "lateral":-1, "turn": 0}, \
+                  'M': {"forward":0, "lateral":0, "turn": 0.5}, \
+                  '<': {"forward":-0.5, "lateral":0, "turn": 0}, \
+                  '>': {"forward":0, "lateral":0, "turn":-0.5}}
     
-    # Walking parameters
-    params = {"Step Height":{"value":0, "min":-1, "max":1, "type":"float"},
+    # BDI Controller bindings 
+    params = {"Forward Stride Length":{ "value":0.15, "min":0, "max":1, \
+                                "type":"float"},
+              "Stride Length Interval":{"value":0.05, "min":0, "max":1, \
+                                "type":"float"},
+              "Lateral Stride Length":{ "value":0.15, "min":0, "max":1, \
+                                "type":"float"},
+              "Step Height":{"value":0, "min":-1, "max":1, "type":"float"}, \
               "Stride Duration":{ "value":0.63, "min": 0, "max":100, \
                                 "type":"float"},
+              "Walk Sequence Length":{"value":5, "min":1, "max":sys.maxint, \
+                                "type":"int"},
+              "Stride Width":{"value":0.2, "min":0, "max":1, "type":"float"},
+              "In Place Turn Size":{"value":math.pi / 16, "min":0, \
+                                    "max":math.pi / 2, "type":"float"},
+              "Turn Radius":{"value":2, "min":0.01, "max":100, "type":"float"},
               "Swing Height":{"value":0.3, "min":0, "max":1, "type":"float"}}
 
     steps = []
 
     
     def init(self):
-        self._isPressing = False;
         self._isWalking = False;
         
         # Connects to necessary command topics
@@ -53,154 +77,13 @@ class AtlasTeleop():
                                     True, None)
         self.control_mode = rospy.Publisher('/atlas/control_mode', 
                                             String, None, False, True, None)
-        self.markers = rospy.Publisher('/steps', MarkerArray)
-        self.feet = rospy.Publisher('/feet', MarkerArray)
-
-        # Connect to TF
-        self.tf = tf.TransformListener()
-
-        # Initialize foot markers
-        self.foot_markers = MarkerArray()
-
-        left_foot = Marker()
-        left_foot.header.frame_id = "world"
-        left_foot.header.stamp = rospy.Time.now()
-        left_foot.ns = "foot"
-        left_foot.id = 0
-        left_foot.action = Marker.ADD
-        left_foot.type = Marker.MESH_RESOURCE
-        left_foot.mesh_resource = "package://atlas/meshes/l_foot.dae"
-        left_foot.lifetime = rospy.Duration.from_sec(0.2)
-        left_foot.scale.x = 1.0
-        left_foot.scale.y = 1.0
-        left_foot.scale.z = 1.0
-        left_foot.color.r = 0.5
-        left_foot.color.g = 0.5
-        left_foot.color.b = 1.0
-        left_foot.color.a = 1.0
-        self.foot_markers.markers.append(left_foot)
-
-        right_foot = Marker()
-        right_foot.header.frame_id = "world"
-        right_foot.header.stamp = rospy.Time.now()
-        right_foot.ns = "foot"
-        right_foot.id = 1
-        right_foot.action = Marker.ADD
-        right_foot.type = Marker.MESH_RESOURCE
-        right_foot.mesh_resource = "package://atlas/meshes/r_foot.dae" 
-        right_foot.lifetime = rospy.Duration.from_sec(0.2)
-        right_foot.scale.x = 1.0
-        right_foot.scale.y = 1.0
-        right_foot.scale.z = 1.0
-        right_foot.color.r = 1.0
-        right_foot.color.g = 0.5
-        right_foot.color.b = 0.5
-        right_foot.color.a = 1.0
-        self.foot_markers.markers.append(right_foot)
-
-        # Wait for transforms
-        self.tf.waitForTransform("world", PADDLE_NAMES[0], rospy.Time.now(), rospy.Duration(1.0))
-        self.tf.waitForTransform("world", PADDLE_NAMES[1], rospy.Time.now(), rospy.Duration(1.0))
-        rospy.loginfo('Starting up leg teleop...')
-
-        # Listen for hydra messages
-        rospy.Subscriber("hydra_calib_throttle", Hydra, self.process_hydra, queue_size=1)
-        rospy.Subscriber("/atlas/atlas_sim_interface_state_throttle", AtlasSimInterfaceState, self.process_atlas, queue_size=1)
-
+        self.service = rospy.Service('walk', Walk, self.walk_service)
 
     def run(self):
         self.init()
-        step_markers = MarkerArray()
-
-        r = rospy.Rate(20)
-        while not rospy.is_shutdown():
-            step_markers.markers = []
-
-            for (i, step) in enumerate(self.steps):
-                marker = Marker()
-                marker.header.frame_id = "world"
-                marker.header.stamp = rospy.Time.now()
-                marker.ns = "step"
-                marker.id = i
-                marker.action = Marker.ADD
-                marker.type = Marker.MESH_RESOURCE
-                marker.mesh_resource = \
-                    "package://atlas/meshes/r_foot.dae" \
-                    if step.foot_index else \
-                    "package://atlas/meshes/l_foot.dae"
-                marker.lifetime = rospy.Duration.from_sec(0.1)
-                marker.pose = step.pose
-                marker.scale.x = 1.0
-                marker.scale.y = 1.0
-                marker.scale.z = 1.0
-                marker.color.r = 0.8 if step.foot_index else 0.0
-                marker.color.g = 0.0
-                marker.color.b = 0.0 if step.foot_index else 0.8
-                marker.color.a = 1.0
-                step_markers.markers.append(marker)
-
-            self.markers.publish(step_markers)
-            r.sleep()
-
-
-    def process_hydra(self, msg):
-        feet = [ None, None ]
-
-        # Drive the hydra around to place footsteps
-        for (i, paddle) in enumerate(msg.paddles):
-            try:
-                (trans,rot) = self.tf.lookupTransform('world', PADDLE_NAMES[i], rospy.Time(0))
-                feet[i] = self.compute_step(i, trans, rot)
-            except (tf.LookupException, 
-                    tf.ConnectivityException, 
-                    tf.ExtrapolationException) as e:
-                rospy.loginfo('Foot placement failure : ' + str(e))
-                return
-            if paddle.buttons[2] and len(self.steps) < NUM_STEPS and not self._isPressing:
-                self._isPressing = True
-                rospy.loginfo('Footstep added: ' + PADDLE_NAMES[i])
-                self.steps.append(feet[i])
-
-        # Publish new footstep positions
-        self.foot_markers.markers[0].pose = feet[0].pose
-        self.foot_markers.markers[1].pose = feet[1].pose
-        self.feet.publish(self.foot_markers)
-
-        # Undo a step if some button is pressed
-        for (i, paddle) in enumerate(msg.paddles):
-            if paddle.buttons[4] and len(self.steps) > 0 and not self._isPressing:
-                self._isPressing = True
-                rospy.loginfo('Last footstep removed.')
-                del self.steps[-1]
-
-        # If we are happy with the footsteps then walk
-        for (i, paddle) in enumerate(msg.paddles):
-            if paddle.buttons[0] and len(self.steps) >= 3 and not self._isPressing:
-                self._isPressing = True
-                rospy.loginfo('Walking! ' + str(len(self.steps)))
-                self._isWalking = True
-                return
-
-        # RESET robot if necessary
-        if msg.paddles[0].buttons[5] and not self._isPressing:
-            self._isPressing = True
-            self._isWalking = False
-            self.steps = []
-            rospy.loginfo('Harnessing robot...')
-            self.reset_to_harnessed()
-            rospy.loginfo('Harnessing complete.')
-        elif msg.paddles[1].buttons[5] and not self._isPressing:
-            self._isPressing = True
-            self._isWalking = False
-            self.steps = []
-            rospy.loginfo('Resetting robot...')
-            self.reset_to_standing()
-            rospy.loginfo('Resetting complete.')
-
-        # If no buttons are pushed, clear the debouncing
-        if not any(msg.paddles[0].buttons) and not any(msg.paddles[1].buttons):
-            self._isPressing = False
-
+        rospy.loginfo('Starting up leg teleop...')
+        rospy.spin()
+        rospy.loginfo('Shutting down leg teleop...')
 
     # Publishes commands to reset robot to a standing position
     def reset_to_harnessed(self):
@@ -208,7 +91,6 @@ class AtlasTeleop():
         self.control_mode.publish("Freeze")
         self.control_mode.publish("StandPrep")
         rospy.sleep(0.3)
-
 
     def reset_to_standing(self):
         self.mode.publish("harnessed")
@@ -219,35 +101,183 @@ class AtlasTeleop():
         rospy.sleep(0.3)
         self.control_mode.publish("Stand")
 
+    def walk_service(self, req):
+        self._isWalking = False;
+        self.steps = []
 
-    # Construct a new step from the hydra's projected ground position
-    def compute_step(self, is_right_foot, trans, rot):
+        for command in req.commands:
+            if self.dynamic_dir.has_key(command):
+                dir = self.dynamic_dir[command]
+                self.steps += self.build_steps(dir["forward"], dir["lateral"], dir["turn"], False)
+            elif self.static_dir.has_key(command):
+                dir = self.dynamic_dir[command]
+                self.steps += self.build_steps(dir["forward"], dir["lateral"], dir["turn"], True)
+
+        self._isWalking = True;
+        rospy.loginfo('Started walk with {0} steps'.format(len(self.steps)))
+        while(self._isWalking):
+            pass
+        rospy.loginfo('Completed walk with {0} steps'.format(len(self.steps)))
+
+        return Empty
+
+    def build_steps(self, forward, lateral, turn, is_static):
+        L = self.params["Forward Stride Length"]["value"]
+        L_lat = self.params["Lateral Stride Length"]["value"]
+        R = self.params["Turn Radius"]["value"]
+        W = self.params["Stride Width"]["value"]
+        X = 0
+        Y = 0
+        theta = 0
+        dTheta = 0
         
-        # Create a new step object
-        step = AtlasBehaviorStepData()
+        if forward != 0:
+            dTheta = turn * 2 * math.asin(L / (2 * (R + \
+            self.params["Stride Width"]["value"]/2)))
+        else:
+            dTheta = turn * self.params["In Place Turn Size"]["value"]
+        steps = []
 
-        # Bookkeeping for step and foot
-        step.step_index = len(self.steps) + 1
+        
+        if forward != 0:
+            dTheta = turn * 2 * math.asin(L / (2 * (R + \
+            self.params["Stride Width"]["value"]/2)))
+        else:
+            dTheta = turn * self.params["In Place Turn Size"]["value"]
+        steps = []
+        
+        # This home step doesn't currently do anything, but it's a 
+        # response to bdi not visiting the first step in a trajectory
+        # home_step = AtlasBehaviorStepData()
+        
+        # If moving right, first dummy step is on the left
+        # home_step.foot_index = 1*(lateral < 0)
+        # home_step.pose.position.y = 0.1
+        # steps.append(home_step)
+        prevX = 0
+        prevY = 0
+        
+        # Builds the sequence of steps needed
+        for i in range(self.params["Walk Sequence Length"]["value"]):
+            # is_right_foot = 1, when stepping with right
+            is_even = i%2
+            is_odd = 1 - is_even
+            is_right_foot = is_even
+            is_left_foot = is_odd
+
+            # left = 1, right = -1            
+            foot = 1 - 2 * is_right_foot
+            
+            if is_static:
+                theta = (turn != 0) * dTheta
+                if turn == 0:
+                    X = (forward != 0) * (forward * L)
+                    Y = (lateral != 0) * (is_odd * lateral * L_lat) + \
+                        foot * W / 2
+                elif forward != 0:
+                    # Radius from point to foot (if turning)
+                    R_foot = R + foot * W/2
+                    
+                    # turn > 0 for CCW, turn < 0 for CW
+                    X = forward * turn * R_foot * math.sin(theta)
+                    Y = forward * turn * (R - R_foot*math.cos(theta))
+                    
+                    rospy.logdebug("R: " + str(R) + " R_foot:" + \
+                    str(R_foot) + " theta: " + str(theta) +  \
+                    " math.sin(theta): " + str(math.sin(theta)) + \
+                    " math.cos(theta) + " + str(math.cos(theta)))
+                elif turn != 0:
+                    X = turn * W/2 * math.sin(theta)
+                    Y = turn * W/2 * math.cos(theta)
+            else:
+                theta += (turn != 0) * dTheta
+                if turn == 0:
+                    X = (forward != 0) * (X + forward * L)
+                    Y = (lateral != 0) * (Y + is_odd * lateral * L_lat) + \
+                        foot * W / 2
+                elif forward != 0:
+                    # Radius from point to foot (if turning)
+                    R_foot = R + foot * W/2
+                    
+                    # turn > 0 for CCW, turn < 0 for CW
+                    X = forward * turn * R_foot * math.sin(theta)
+                    Y = forward * turn * (R - R_foot*math.cos(theta))
+                    
+                    rospy.logdebug("R: " + str(R) + " R_foot:" + \
+                    str(R_foot) + " theta: " + str(theta) +  \
+                    " math.sin(theta): " + str(math.sin(theta)) + \
+                    " math.cos(theta) + " + str(math.cos(theta)))
+                elif turn != 0:
+                    X = turn * W/2 * math.sin(theta)
+                    Y = turn * W/2 * math.cos(theta)
+                    
+             
+            Q = quaternion_from_euler(0, 0, theta)
+            step = AtlasBehaviorStepData()
+            
+            # One step already exists, so add one to index
+            step.step_index = i
+            
+            # Alternate between feet, start with left
+            step.foot_index = is_right_foot
+            
+            #If moving laterally to the left, start with the right foot
+            if (lateral > 0):
+                step.foot_index = is_left_foot
+            
+            step.duration = self.params["Stride Duration"]["value"]
+            
+            step.pose.position.x = X
+            step.pose.position.y = Y
+            step.pose.position.z = self.params["Step Height"]["value"]
+         
+            step.pose.orientation.x = Q[0]
+            step.pose.orientation.y = Q[1]
+            step.pose.orientation.z = Q[2]
+            step.pose.orientation.w = Q[3]
+            
+            step.swing_height = self.params["Swing Height"]["value"]         
+            steps.append(step)
+        
+        # Add final step to bring feet together
+        is_right_foot = 1 - steps[-1].foot_index
+        is_even = is_right_foot
+        # foot = 1 for left, foot = -1 for right
+        foot = 1 - 2 * is_right_foot
+        
+        if turn == 0:
+            Y = Y + foot * W
+        elif forward != 0:
+            rospy.logdebug("R: " + str(R) + " R_foot:" + \
+            str(R_foot) + " theta: " + str(theta) +  \
+           " math.sin(theta): " + str(math.sin(theta)) + \
+           " math.cos(theta) + " + str(math.cos(theta)))
+            
+            # R_foot is radius to foot
+            R_foot = R + foot * W/2
+            #turn > 0 for counter clockwise
+            X = forward * turn * R_foot * math.sin(theta)
+            Y = forward * turn * (R - R_foot*math.cos(theta))
+        else:
+            X = turn * W/2 * math.sin(theta)
+            Y = turn * W/2 * math.cos(theta)
+            
+        Q = quaternion_from_euler(0, 0, theta)
+        step = AtlasBehaviorStepData()
+        step.step_index = len(steps)
         step.foot_index = is_right_foot
         step.duration = self.params["Stride Duration"]["value"]
-        step.swing_height = self.params["Swing Height"]["value"]
-            
-        # Project foot position into XY plane
-        step.pose.position.x = trans[0]
-        step.pose.position.y = trans[1]
+        step.pose.position.x = X
+        step.pose.position.y = Y
         step.pose.position.z = self.params["Step Height"]["value"]
-         
-        # Project rotation into XY plane
-        (roll, pitch, yaw) = euler_from_quaternion(rot, axes='sxyz')
-        Q = quaternion_from_euler(0, 0, yaw, axes='sxyz')
-
         step.pose.orientation.x = Q[0]
         step.pose.orientation.y = Q[1]
         step.pose.orientation.z = Q[2]
         step.pose.orientation.w = Q[3]
-
-        # Return computed position
-        return step
+        step.swing_height = self.params["Swing Height"]["value"]
+        
+        steps.append(step)
+        return steps
         
     # Send a trajectory of step commands when walking
     def process_atlas(self, state):
