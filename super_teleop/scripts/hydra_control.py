f17b5c8fd50dee929ecaf1d72a28c2abd38c0ec5
Pras Velagapudi <psigen@gmail.com>
2013-06-15 23:46:04 -0400

    Fixed a number of issues, got to the point where calls are being made.

--- super_teleop/scripts/hydra_control.py
+++ super_teleop/scripts/hydra_control.py
@@ -3,7 +3,7 @@ import roslib; roslib.load_manifest('super_teleop')
 
 from razer_hydra.msg import Hydra
 from std_msgs.msg import String
-from atlas_replay.srv import Record, Play
+from atlas_replay.srv import Record, RecordRequest, Play
 import tf
 import rospy
 
@@ -29,23 +29,26 @@ class HydraControl():
         rospy.wait_for_service('play')
         self.play = rospy.ServiceProxy('play', Play)
 
-        self.record_msg = Record()
+        self.record_msg = RecordRequest()
         self.input = rospy.Subscriber("hydra_calib", Hydra, self.process_hydra)
     
     def run(self):
         self.init()
+        print 'Starting hydra control...'
         rospy.spin()
+        print 'Stopping hydra control...'
 
     def process_hydra(self, msg):
         if not self.prev_msg:
             self.prev_msg = msg
             return
 
-        # Get paddle states
+        # Get paddle states and overwrite prev message
         left = msg.paddles[0]
         right = msg.paddles[1]
         left_old = self.prev_msg.paddles[0]
-        left_old = self.prev_msg.paddles[1]
+        right_old = self.prev_msg.paddles[1]
+        self.prev_msg = msg
 
         # Left paddle bindings
         if left.buttons[0] and not left_old.buttons[0] and not self.record_msg.record:
@@ -57,25 +60,25 @@ class HydraControl():
             except rospy.ServiceException, e:
                 print "Execution command failed: %s" % str(e)
 
-        if left.buttons[1] and not left_old.buttons[1]:
-            self.record_msg.left_leg = not self.record_msg.left_leg
+        if not self.record_msg.record:
+            if left.buttons[1] and not left_old.buttons[1]:
+                self.record_msg.left_leg = not self.record_msg.left_leg
 
-        if left.buttons[2] and not left_old.buttons[2]:
-            self.record_msg.right_leg = not self.record_msg.right_leg
+            if left.buttons[2] and not left_old.buttons[2]:
+                self.record_msg.right_leg = not self.record_msg.right_leg
 
-        if left.buttons[3] and not left_old.buttons[3]:
-            self.record_msg.left_arm = not self.record_msg.left_arm
+            if left.buttons[3] and not left_old.buttons[3]:
+                self.record_msg.left_arm = not self.record_msg.left_arm
 
-        if left.buttons[4] and not left_old.buttons[4]:
-            self.record_msg.right_arm = not self.record_msg.right_arm
+            if left.buttons[4] and not left_old.buttons[4]:
+                self.record_msg.right_arm = not self.record_msg.right_arm
             
-        if left.buttons[5] and not left_old.buttons[5]:
-            self.record_msg.torso = not self.record_msg.torso
+            if left.buttons[5] and not left_old.buttons[5]:
+                self.record_msg.torso = not self.record_msg.torso
                        
         if left.trigger > 0.9 and left_old.trigger <= 0.9:
             self.record_msg.record = not self.record_msg.record
             try:
-                print "Recording: %s" % str(self.record_msg.record)
                 self.record(self.record_msg)
             except rospy.ServiceException, e:
                 print "Recording command failed: %s" % str(e)
