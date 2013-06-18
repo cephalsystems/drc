fea5da75aef9b5b8e95f222d1bdc036f31dedecc
Pras Velagapudi <psigen@gmail.com>
2013-06-18 01:01:57 -0400

    Added new string command stuff.

---
+++ msgpack_streamer/src/string_stream.cpp
@@ -0,0 +1,88 @@
+/**
+ * Creates a node that reads PCL2 data and streams it over a TCP socket
+ * to any particular listeners.
+ */
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <msgpack.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/foreach.hpp>
+#include <vector>
+
+#define STREAM_TYPE std_msgs::String
+
+using boost::asio::ip::tcp; 
+
+std::vector<boost::shared_ptr<tcp::iostream> > _streams;
+boost::mutex _stream_lock;
+
+void broadcast(msgpack::sbuffer &buffer) 
+{
+    boost::lock_guard<boost::mutex> lock(_stream_lock);
+    BOOST_FOREACH( boost::shared_ptr<tcp::iostream> stream, _streams ) {
+      stream->write(buffer.data(), buffer.size());
+      stream->flush();
+    }
+}
+
+void callback(const boost::shared_ptr<const STREAM_TYPE> &msg) 
+{
+  msgpack::sbuffer buffer;
+  msgpack::packer<msgpack::sbuffer> pk(&buffer);
+
+  pk.pack(msg->data);
+  
+  // Send this data to each client
+  broadcast(buffer);
+}
+
+void serverThreadFn(boost::asio::io_service& io_service, int port) 
+{
+  tcp::endpoint endpoint(tcp::v4(), port);
+  tcp::acceptor acceptor(io_service, endpoint);
+  acceptor.set_option(boost::asio::ip::tcp::no_delay(false));
+  
+  while (ros::ok()) {
+    boost::shared_ptr<tcp::iostream> stream = boost::make_shared<tcp::iostream>();
+    boost::system::error_code ec;    
+    acceptor.accept(*stream->rdbuf(), ec);
+
+    if (ec) {
+      ROS_WARN("Connection failed: %s", ec.message().c_str());
+      continue;
+    } else {
+      boost::lock_guard<boost::mutex> lock(_stream_lock);
+      _streams.push_back(stream);
+    }    
+  }
+}
+
+int main(int argc, char **argv)
+{
+  // Initialize ROS node
+  ros::init(argc, argv, "image_stream");
+  ros::NodeHandle nh;
+  ros::NodeHandle nhp("~");
+
+  // Subscribe to a point cloud stream
+  ros::Subscriber sub = nh.subscribe<STREAM_TYPE>("commands", 10, callback);
+
+  // Open a TCP socket
+  int stream_port;
+  nhp.param<int>("port", stream_port, 6666);
+  boost::asio::io_service io_service;
+  boost::thread server_thread(serverThreadFn, boost::ref(io_service), stream_port);
+
+  // Update ROS
+  ROS_INFO("Starting BSON stream: %s -> [%d]", 
+	   nh.resolveName("image").c_str(), stream_port);
+  ros::spin();
+  ROS_INFO("Stopping BSON stream: %s -> [%d]", 
+	   nh.resolveName("image").c_str(), stream_port);
+
+  // Shutdown boost.asio
+  io_service.stop();
+}
